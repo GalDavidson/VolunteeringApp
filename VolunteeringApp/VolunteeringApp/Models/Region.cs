23ee3a4dfddbdf093564a97d2cb2cf707999687d
GalDavidson <gal.davidson8@gmail.com>
2022-04-27 09:39:59 +0300

    .

---
+++ VolunteeringApp/VolunteeringApp/Models/Region.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VolunteeringApp.Models
+{
+    public partial class Region
+    {
+        public Region()
+        {
+            DailyEvents = new List<DailyEvent>();
+        }
+
+        public int RegionId { get; set; }
+        public string RegionName { get; set; }
+
+        public virtual List<DailyEvent> DailyEvents { get; set; }
+    }
+}
