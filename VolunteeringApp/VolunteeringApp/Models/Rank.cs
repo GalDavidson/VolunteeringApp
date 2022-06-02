8baae8c14c184a023a0d5f86082bdbfbf2d4b33a
GalDavidson <gal.davidson8@gmail.com>
2022-06-02 17:27:18 +0300

    .

---
+++ VolunteeringApp/VolunteeringApp/Models/Rank.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VolunteeringApp.Models
+{
+    public partial class Rank
+    {
+        public int RankId { get; set; }
+        public string RankName { get; set; }
+    }
+}
